c640d9f778e48697270580bbfedd2f40549a2109
mutwiri-2 <devmutwiri2@gmail.com>
2021-02-11 23:38:17 +0300

    variable declaration in php

--- 02_variables.php
+++ 02_variables.php
@@ -2,6 +2,16 @@
 
 // What is a variable
 
+echo "A variable is a container that stores some value." . '<br>';
+echo "PHP is a loosely typed language meaning that variables do not have types.
+The programmer does not specify the type when declaring variables and the
+variables have dynamic types i.e their type changes based on the value assigned.";
+echo '<br>' . "Variable names must start with letter or underscore and can
+ contain digits as well" . '<br>';
+echo "Syntax of declaring a variable is \$variable_name = value;".'<br>';
+
+
+
 // Variable types
 
 // Declare variables
@@ -23,3 +33,4 @@
 // Constants
 
 // Using PHP built-in constants
+?>
