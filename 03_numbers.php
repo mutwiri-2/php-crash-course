2f41fd0059647131259409d3267081911683a41c
mutwiri-2 <devmutwiri2@gmail.com>
2021-02-13 10:12:08 +0300

    Conversion from string to numeric value

--- 03_numbers.php
+++ 03_numbers.php
@@ -28,8 +28,26 @@ echo $a-- . '<br>'; //prints value first then increases it
 echo --$a . '<br>'; //increases value first then prints it
 
 // Number checking functions
+echo is_float(1.25) . '<br>';
+echo is_double(455531.87) . '<br>';
+echo is_int(-23) . '<br>';
+echo is_integer(10) . '<br>';
+echo is_numeric("3.45") . '<br>';
+echo var_dump(is_numeric("3g.09")) . '<br>';
 
 // Conversion
+$strNumber = '12.34';
+$num = (float)$strNumber;
+var_dump($num);
+$number = (int)$strNumber; //drops everything after decimal
+var_dump($number);
+
+$stringNum = '45.09';
+$numFloat = floatval($stringNum);
+var_dump($numFloat);
+$numInt = intval($stringNum);
+var_dump($numInt);
+
 
 // Number functions
 
