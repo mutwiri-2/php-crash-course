11bdd1e0e125a68f1440b4c7610f95b14b00f67e
mutwiri-2 <devmutwiri2@gmail.com>
2021-02-13 11:25:41 +0300

    useful string functions

--- 04_strings.php
+++ 04_strings.php
@@ -14,6 +14,27 @@ echo $string1;
 echo 'Hello' . ' World' . ' PHP' .'<br>';
 
 // String functions
+$str = "    Hello World     ";
+
+echo "1 -- " . strlen($str) . '<br>';
+echo "2 -- " . trim($str) . '<br>';
+echo "3 -- " . ltrim($str) . '<br>';
+echo "4 -- " . rtrim($str) . '<br>';
+echo "5 -- " . str_word_count($str) . '<br>'; //number of words
+echo "6 -- " . strrev($str) . '<br>';
+echo "7 -- " . strtoupper($str) . '<br>';
+echo "8 -- " . strtolower($str) . '<br>';
+echo "9 -- " . ucfirst('hello world') . '<br>'; // converts first letter of first word
+echo "10 -- " . lcfirst('Hello World') . '<br>'; // converts first letter of first word
+echo "11 -- " . ucwords('hello world from kenya') . '<br>';
+echo "12 -- " . strpos($str, 'world') . '<br>';
+echo "13 -- " . stripos($str, 'world') . '<br>';
+echo "14 -- " . substr($str, 0) . '<br>';
+echo "15 -- " . substr($str, 2) . '<br>';
+echo "16 -- " . substr($str, 2, 12) . '<br>';
+echo "17 -- " . str_replace('world', 'PHP', $str) . '<br>';
+echo "18 -- " . str_ireplace('world', 'PHP', $str) . '<br>';
+
 
 // Multiline text and line breaks
 
