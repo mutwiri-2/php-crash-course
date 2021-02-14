305d2b8f750acd9845700fe5b06a670a9349977c
mutwiri-2 <devmutwiri2@gmail.com>
2021-02-14 23:53:04 +0300

    two-dimensional arrays

--- 05_arrays.php
+++ 05_arrays.php
@@ -194,3 +194,10 @@ var_dump($person);
 echo '</pre>';
 
 // Two dimensional arrays
+$todos = [
+    ['title' => 'Todo title 1', 'completed' => true],
+    ['title' => 'Todo title 2', 'completed' => false]
+];
+echo '<pre>';
+var_dump($todos);
+echo '</pre>';
