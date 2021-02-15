1eedadfa677ff4b3ac6e1c0d3b9f78e90f83267b
mutwiri-2 <devmutwiri2@gmail.com>
2021-02-15 14:02:29 +0300

    sum of two numbers function

--- 08_functions.php
+++ 08_functions.php
@@ -17,6 +17,11 @@ echo hello1('Mutwiri');
 echo hello1('Mwenda');
 
 // Create sum of two function
+function sumOfTwo($num1, $num2)
+{
+    return $num1 + $num2;
+}
+echo sumOfTwo(90, 20);
 
 // Create function to sum all numbers using ...$nums
 
