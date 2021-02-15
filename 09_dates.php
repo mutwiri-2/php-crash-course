bedd3d9ab4a24f0d03c1efa33e14ca3823f7bbd7
mutwiri-2 <devmutwiri2@gmail.com>
2021-02-15 15:40:08 +0300

    parse dates from format and from input

--- 09_dates.php
+++ 09_dates.php
@@ -14,5 +14,14 @@ echo date('F j Y H:i:s') . '<br>';
 echo time(); // seconds from 1970 Jan 1st midnight
 
 // Parse date: https://www.php.net/manual/en/function.date-parse.php
+$parsedDate = date_parse('2020-10-12 09:00:00');
+echo '<pre>';
+var_dump($parsedDate);
+echo '</pre>';
 
 // Parse date from format: https://www.php.net/manual/en/function.date-parse-from-format.php
+$dateString = 'February 15 2021 12:45:33';
+$parsedDate = date_parse_from_format('F j Y H:i:s', $dateString);
+echo '<pre>';
+var_dump($parsedDate);
+echo '</pre>';
