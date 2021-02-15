788c9c1e5ce89268619d79b23aa7383f55962b49
mutwiri-2 <devmutwiri2@gmail.com>
2021-02-15 15:30:13 +0300

    different date formats

--- 09_dates.php
+++ 09_dates.php
@@ -4,9 +4,11 @@
 echo date('Y-m-d H:i:s') . '<br>';
 
 // Print yesterday
-echo date('Y-m-d H:i:s', time() - 60*60*24) . '<br>';;
+echo date('Y-m-d H:i:s', time() - 60*60*24) . '<br>';
 
 // Different format: https://www.php.net/manual/en/function.date.php
+echo date('l jS \of F Y h:i:s A') . '<br>';
+echo date('F j Y H:i:s') . '<br>';
 
 // Print current timestamp
 
