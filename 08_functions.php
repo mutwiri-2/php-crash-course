1e62b734b96d59e713893b9c341274b5e2b17783
mutwiri-2 <devmutwiri2@gmail.com>
2021-02-15 13:54:47 +0300

    declare and call a function with arguments

--- 08_functions.php
+++ 08_functions.php
@@ -7,9 +7,16 @@ function hello()
 }
 hello();
 
-// Function with argument
+// Function with argument and return statement
+function hello1($name)
+{
+    return "Hello $name" . '<br>';
+}
+echo hello1('Kevin');
+echo hello1('Mutwiri');
+echo hello1('Mwenda');
 
-// Create sum of two functions
+// Create sum of two function
 
 // Create function to sum all numbers using ...$nums
 
