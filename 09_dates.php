2b812ebafa9737494c6bb209aa6a22376e46a512
mutwiri-2 <devmutwiri2@gmail.com>
2021-02-15 15:22:21 +0300

    print yesterday

--- 09_dates.php
+++ 09_dates.php
@@ -1,9 +1,10 @@
 <?php
 
 // Print current date
-echo date('Y-m-d H:i:s');
+echo date('Y-m-d H:i:s') . '<br>';
 
 // Print yesterday
+echo date('Y-m-d H:i:s', time() - 60*60*24) . '<br>';;
 
 // Different format: https://www.php.net/manual/en/function.date.php
 
