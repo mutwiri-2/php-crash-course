cd118520ef95427cd6f3ce760f22da623f8ebd63
mutwiri-2 <devmutwiri2@gmail.com>
2021-02-15 17:19:29 +0300

    scan dir for files and folders - current, parent

--- 11_fs/index.php
+++ 11_fs/index.php
@@ -1,13 +1,28 @@
 <?php
-// Magic constants
+// Magic constants - constants that change their value based on the execution context
+echo __DIR__ . '<br>'; // prints current directory of file
+echo __FILE__ . '<br>'; // prints current file
+echo __LINE__ . '<br>'; // prints the line number executing
 
 // Create directory
+mkdir('test');
 
 // Rename directory
+rename('test', 'test1');
 
 // Delete directory
+rmdir('test1');
 
 // Read files and folders inside directory
+$files = scandir('./'); //current directory
+echo '<pre>';
+var_dump($files);
+echo '</pre>';
+
+$files = scandir('../'); //parent directory
+echo '<pre>';
+var_dump($files);
+echo '</pre>';
 
 // file_get_contents, file_put_contents
 
