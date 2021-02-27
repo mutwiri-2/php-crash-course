79a8ac8596f9fd5aff60a17e410f440fdd83b2c2
mutwiri-2 <devmutwiri2@gmail.com>
2021-02-27 07:00:05 +0300

    make connection to database

--- 14_product_crud/index.php
+++ 14_product_crud/index.php
@@ -1,3 +1,9 @@
+<?php
+    // make connection to database
+    $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', 'admin1424');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -9,7 +15,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" href="./app.css">
 
-    <title>Hello, world!</title>
+    <title>Products CRUD App</title>
   </head>
   <body>
     <h1>Products CRUD App</h1>
