fed45c68252f1d9a0dd2984d0fafbd4f82257785
mutwiri-2 <devmutwiri2@gmail.com>
2021-02-27 04:38:52 +0300

    create a resource, set options and execute to get result

--- 13_curl/index.php
+++ 13_curl/index.php
@@ -1,7 +1,21 @@
 <?php
 
+// what is curl?
+echo "curl is a tool that gives you ability to interact remotely with other
+services or other resources".'<br>';
+
 $url = 'https://jsonplaceholder.typicode.com/users';
+
 // Sample example to get data.
+// start the resource
+$resource = curl_init($url);
+
+//set options
+curl_setopt($resource, CURLOPT_RETURNTRANSFER, true);
+
+// curl_exec on resource to get a result 
+$result = curl_exec($resource);
+echo $result;
 
 // Get response status code
 
