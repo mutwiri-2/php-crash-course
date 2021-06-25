358d48f57c59f7fc800e47322eb10b94aedcfccd
mutwiri-2 <devmutwiri2@gmail.com>
2021-06-25 12:25:03 +0300

    fix wording

--- 10_include_require/01_website_skeleton/index.php
+++ 10_include_require/01_website_skeleton/index.php
@@ -4,7 +4,7 @@
 
 <!-- difference between include and require -->
 <!-- When the file is not found using require it basically stops
-the execution of the file. You should use require when that
+the execution of the rest of the code. You should use require when that
 file is absolutely necesary for the page's functionality
 
 When the file is not found using include the rest of the code still executes.
