aa990923f4907e05d99fc036f1431a0284f4df14
mutwiri-2 <devmutwiri2@gmail.com>
2021-07-07 11:07:58 +0300

    iterate over each product and display on table

--- 14_product_crud/index.php
+++ 14_product_crud/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 // connect to database using PDO instead of mysqli - more powerful, supports multiple dbs & ObjOriented
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_CRUD;', 'root', 'admin1424'); //DSN string, user, password
@@ -32,33 +32,34 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC); // returns each product as a
 
 <body>
     <h1>Products CRUD</h1>
+
     <table class="table">
         <thead>
             <tr>
                 <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">Image</th>
+                <th scope="col">Title</th>
+                <th scope="col">Price</th>
+                <th scope="col">Date Added</th>
+                <th scope="col">Action</th>
+
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-            </tr>
+            <?php foreach ($products as $i => $product) : ?>
+                <tr>
+                    <th scope="row"><?php echo $i+1; ?></th>
+                    <td></td>
+                    <td><?php echo $product['title']; ?></td>
+                    <td><?php echo $product['price']; ?></td>
+                    <td><?php echo $product['create_date']; ?></td>
+                    <td>
+                    <button type="button" class="btn-sm btn-outline-primary">Edit</button>
+                    <button type="button" class="btn-sm btn-outline-danger">Delete</button>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+
         </tbody>
     </table>
 </body>
